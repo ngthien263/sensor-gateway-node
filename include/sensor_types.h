307c7745f073dd70c00d2e6c0e4f3af00e6b0890
ngthien263 <vuongngocthien222@gmail.com>
2026-02-15 17:15:16 +0000

    refactor: replace sensor state flags with bool fields

--- include/sensor_types.h
+++ include/sensor_types.h
@@ -4,6 +4,8 @@
 #include <time.h>
 #include <stdio.h>      
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 #define BUFF_SIZE 256
 #define MAX_SENSORS 10
 #define TEMP_UPPER_LIMIT 50
@@ -15,15 +17,21 @@ typedef struct {
 } sensor_data_t;
 
 typedef struct {
-    int in_use;
-    int connected;
+    bool new;
+    bool connected;
 } sensor_state_t;
 
-typedef struct {
-    int id;
-    sensor_data_t data;
+typedef struct __attribute__((packed)){
     char timestamp[64];
+    uint16_t id;
+    sensor_data_t data;
     sensor_state_t state;
 } sensor_info_t;
+
+typedef struct __attribute__((packed)) {
+    uint32_t timestamp;
+    uint16_t id;
+    sensor_data_t data;
+} sensor_packet_t;
 
 #endif 
