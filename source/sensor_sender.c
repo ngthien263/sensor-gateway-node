b4d764075a8ca9ac41eaf0ebd061b28a47de30a2
ngthien263 <vuongngocthien222@gmail.com>
2026-02-15 17:15:58 +0000

    feat: switch sensor communication from text-based to binary packet

--- source/sensor_sender.c
+++ source/sensor_sender.c
@@ -3,22 +3,10 @@
 #include <sys/time.h>
 #include "sensor_sender.h"
 #include "common.h"
-static void get_time(char *buffer) {
-    time_t t = time(NULL);
-    struct tm* timeinfo = localtime(&t);
-    snprintf(buffer, 64, "%02d:%02d:%02d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-}
-
-sensor_info_t sensor_init(int sensor_id){
-    sensor_info_t new_sensor = {0};
-    new_sensor.id = sensor_id;
-    get_time(new_sensor.timestamp);
-    return new_sensor;
-}
 
 sensor_node_t* sensor_create(int sensor_id){
     sensor_node_t* new_sensor = (sensor_node_t*)malloc(sizeof(sensor_node_t));
-    new_sensor->info = sensor_init(sensor_id);
+    new_sensor->id = sensor_id;
     new_sensor->next = NULL;
     return new_sensor;
 }
@@ -40,7 +28,7 @@ void rm_sensor(sensor_node_t** head_sensor, int sensor_id){
     sensor_node_t* curr = *head_sensor;
     sensor_node_t* prev = NULL;
     while (curr != NULL) {
-        if(curr->info.id == sensor_id){
+        if(curr->id == sensor_id){
             if(prev == NULL) {
                 *head_sensor = curr->next;
             } else
@@ -59,38 +47,29 @@ void dump_sensor(sensor_node_t** head_sensor) {
     int count = 1;
     sensor_node_t* temp = *head_sensor;
     while(temp != NULL){
-        printf("%d. ID: %d\n", count++, temp->info.id);
+        printf("%d. ID: %d\n", count++, temp->id);
         temp = temp->next; 
     }
 }
 
-sensor_info_t* stimulate_sensor_info(sensor_info_t* self){
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    srand((unsigned int)(tv.tv_sec ^ tv.tv_usec));
-    self->data.temperature = rand() % 100 + 1;
-    self->data.humidity = rand() % 100 + 1;
-    get_time(self->timestamp);
-    return self;
+static void generate_packet(int id, sensor_packet_t* pkt)
+{
+    pkt->timestamp   = (uint32_t)time(NULL);
+    pkt->id          = id;
+    pkt->data.temperature = rand() % 100 + 1;
+    pkt->data.humidity    = rand() % 100 + 1;
 }
-
-void sensor_generate_message(sensor_info_t* self, char* buff){
-    memset(buff, 0, BUFF_SIZE);
-    sensor_info_t* sensor_data = stimulate_sensor_info(self);
-    snprintf(buff,BUFF_SIZE, "%s - ID: %dTemp: %.2f Humid: %.2f\n", sensor_data->timestamp,
-                                                         self->id,
-                                                         sensor_data->data.temperature,
-                                                         sensor_data->data.humidity);
-}
-
 
 void sensor_send_message(sensor_node_t** sensor_head, int server_fd) {
-    char buff[BUFF_SIZE];
+    sensor_packet_t pkt;
+    
     sensor_node_t* curr = *sensor_head;
     while(curr != NULL){
-        sensor_generate_message(&curr->info, buff);
-        if(write(server_fd, buff, strlen(buff)) == -1)
-             handle_error("write()");
+        generate_packet(curr->id, &pkt);
+
+        if(write(server_fd, &pkt, sizeof(pkt)) != sizeof(pkt))
+            handle_error("write()");
+        
         curr = curr->next;
     }
 }
